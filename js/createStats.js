ee413b28633bfb71d021483e65d72b481eb75506
Lucia Kocincova <lucia.kocincova@qb0006-win.quantumblack.local>
2016-07-21 16:36:02 +0100

    Added stats and dat.gui
    Added statistics about rendering and controls generated by dat.gui

---
+++ js/createStats.js
@@ -0,0 +1,10 @@
+function createStats(){
+  var stats = new Stats();
+  stats.setMode(0);
+
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0';
+  stats.domElement.style.top = '0';
+
+  return stats;
+}
--- js/demo_01.js
+++ js/demo_01.js
@@ -2,50 +2,75 @@ var scene;
 var camera;
 var renderer;
 
+var stats;
+var control;
+
 var geometry;
 var material;
 
 init();
-animate();
+render();
 
 function init() {
+  scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+  var fieldOfView = 75;
+  var aspect = window.innerWidth / window.innerHeight;
+  var near = 1;
+  var far = 10000;
+  camera = new THREE.PerspectiveCamera( fieldOfView, aspect, near, far );
+  camera.position.z = 1000;
 
-    var fieldOfView = 75;
-    var aspect = window.innerWidth / window.innerHeight;
-    var near = 1;
-    var far = 10000;
-    camera = new THREE.PerspectiveCamera( fieldOfView, aspect, near, far );
-    camera.position.z = 1000;
+  geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+  var mesh = new THREE.Mesh( geometry, material );
+  mesh.name = 'cube';
+  scene.add( mesh );
 
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.name = 'cube';
-    scene.add( mesh );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
 
-    document.body.appendChild( renderer.domElement );
+  stats = createStats();
+  document.body.appendChild( stats.domElement );
+
+  control = new function () {
+    this.rotationSpeed = 0.005;
+    this.scale = 1;
+  };
+
+  addControls( control );
 
 }
 
-function animate() {
+function render() {
 
-    requestAnimationFrame( animate );
+  requestAnimationFrame( render );
 
-    moveCube();
+  moveCube();
 
-    renderer.render( scene, camera );
+  renderer.render( scene, camera );
+
+  stats.update();
 
 }
 
 function moveCube(){
 
-  scene.getObjectByName('cube').rotation.x += 0.01;
-  scene.getObjectByName('cube').rotation.y += 0.02;
-  
+  scene.getObjectByName('cube').rotation.x += control.rotationSpeed;
+  scene.getObjectByName('cube').rotation.y += control.rotationSpeed;
+
+  var s = control.scale;
+  scene.getObjectByName('cube').scale.set( s, s, s);
+
+}
+
+function addControls( controlObj ) {
+
+  var gui = new dat.GUI();
+  gui.add( controlObj, 'rotationSpeed', -0.1, 0.1 );
+  gui.add( controlObj, 'scale', 0.01, 2);
+
 }
